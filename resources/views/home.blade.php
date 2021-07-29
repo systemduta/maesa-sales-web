e5ea9690d77118112d2c44149366622eae4bb6ec
systemduta <systemduta@gmail.com>
2021-07-29 22:44:40 +0700

    fixing cant load image on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,7 +48,7 @@
             <ul class="users-list clearfix">
               @foreach ($users as $key => $user)
                 <li>
-                  <img src="{{ $user->avatar }}" alt="User Image">
+                  <img src="/storage/{{ $user->avatar }}" alt="User Image">
                   <a class="users-list-name" href="javascript:void(0)" data-toggle="modal" data-target="#user-modal-{{$key}}">{{ $user->name }}</a>
                   <span class="users-list-date">{{ $user->nik }}</span>
                 </li>
@@ -63,7 +63,7 @@
                           </button>
                         </div>
                         <div class="modal-body" style="padding-left:2rem; padding-right:2rem;">
-                          <img src="{{ $user->avatar }}" class="rounded-circle mx-auto d-block" style="height: 10rem; width:10rem;" alt="Product Image">
+                          <img src="/storage/{{ $user->avatar }}" class="rounded-circle mx-auto d-block" style="height: 10rem; width:10rem;" alt="Product Image">
                           <div style="height: 2rem;"></div>
                           <p class="font-weight-bold">Name</p>
                           <p>{{$user->name}}</p>
@@ -77,7 +77,7 @@
                       </div> <!-- /.modal-content -->
                     </div> <!-- /.modal-dialog -->
                   </div> <!-- /.modal POP UP USER -->
-                
+
               @endforeach
             </ul>
             <!-- /.users-list -->
@@ -103,7 +103,7 @@
                 @foreach ($products as $key => $product)
                   <li class="item">
                     <div class="product-img">
-                      <img src= "{{ $product->img }}" alt="Product Image" class="img-size-50">
+                      <img src= "/storage/{{ $product->img }}" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
                       <a href="javascript:void(0)" class="product-title" data-toggle="modal" data-target="#product-modal-{{$key}}"> {{ $product->name }}
@@ -124,8 +124,8 @@
                             </button>
                           </div>
                         <div class="modal-body">
-                          <div class="row"> 
-                            <img src="{{ $product->img }}" class="rounded mx-auto d-block" style="height: 10rem; width:10rem;" alt="Product Image">
+                          <div class="row">
+                            <img src="/storage/{{ $product->img }}" class="rounded mx-auto d-block" style="height: 10rem; width:10rem;" alt="Product Image">
                           </div>
                           <div style="height: 2rem;"></div>
                           <div class="form-group row">
