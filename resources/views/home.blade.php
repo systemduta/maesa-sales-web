2010ad7ca64a6de2369a514405346d9c24f4f920
dethaa <sharonmarbun12@gmail.com>
2021-07-30 14:43:47 +0700

    More info anchor href to /transactions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="/transactions" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div> <!-- ./col -->
     </div> <!-- ./row -->
