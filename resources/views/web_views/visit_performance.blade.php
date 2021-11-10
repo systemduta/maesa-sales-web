636c9ecc84c32a334f5a88d0d51422672ef32f94
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2021-11-10 15:33:44 +0700

    update visit performance chart

--- resources/views/web_views/visit_performance.blade.php
+++ resources/views/web_views/visit_performance.blade.php
@@ -6,17 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Visit Performance</title>
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600,800,900" rel="stylesheet" type="text/css">
-{{--    <style>--}}
-{{--        #container {--}}
-{{--            margin: 20px;--}}
-{{--            width: 100%;--}}
-{{--            height: 100vh;--}}
-{{--        }--}}
-{{--    </style>--}}
+    <style>
+        #container {
+            margin: 20px;
+            width: 200px;
+            height: 100px;
+        }
+    </style>
 </head>
 
 <body>
+<div style="display: flex; justify-content: center;">
 <div id="container"></div>
+</div>
 <script src="{{ asset('AdminLTE') }}/plugins/chart/progressbar.min.js"></script>
 <script>
     var achieved = {{ app('request')->input('achieved') ?? 0 }};
@@ -36,23 +38,23 @@
             value: '',
             alignToBottom: false
         },
-        from: {color: '#FFEA82'},
-        to: {color: '#ED6A5A'},
+        from: {color: '#e71a1a'},
+        to: {color: '#5aed72'},
         // Set default step function for all animate calls
         step: (state, bar) => {
             bar.path.setAttribute('stroke', state.color);
             var value = Math.round(bar.value() * 100);
             if (value === 0) {
-                bar.setText('');
+                bar.setText('0%');
             } else {
-                bar.setText(value);
+                bar.setText(value + '%');
             }
 
             bar.text.style.color = state.color;
         }
     });
     bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-    bar.text.style.fontSize = '3rem';
+    bar.text.style.fontSize = '2rem';
     bar.text.style.fontWeight = 'bold';
 
     bar.animate(percent);  // Number from 0.0 to 1.0
